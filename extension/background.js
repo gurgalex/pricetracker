7ab1cde5694702f047a105c716d53449499e46a4
Alex Gurganus <gurgalex@iastate.edu>
2020-05-07 00:24:37 -0500

    Gather initial desired info from API url

--- extension/background.js
+++ extension/background.js
@@ -22,10 +22,41 @@ chrome.webRequest.onCompleted.addListener((details) => {
             .then(response => response.json())
             .then(data => {
                 console.log("captured request for url details: ", details);
-                console.log("product info: ", data)
+                console.log("product info: ", data);
+                let parsed_product_info = new ProductInfo(data);
+                console.log("formatted product info:", parsed_product_info,
+                    "\nitem name:", parsed_product_info.title,
+                    "\ntotal price:", parsed_product_info.price.list_price,
+                    "\n$ per unit (" + parsed_product_info.price.priceUnitOfMeasure + ")" + ":", parsed_product_info.price.unit_price,
+                    "\ndate recorded:", (new Date()).toUTCString()
+                );
             });
     }, {urls: ["*://grocery.walmart.com/v3/api/products/*"],
         types: [
             "xmlhttprequest", // capture itemFields request
             ]}
 );
+
+/**
+ * Detach the grocerry store's internal data structure from ours incase their's changes.
+ */
+class ProductInfo {
+    constructor(productJson) {
+        this._json = productJson;
+        // Title of product
+        this.title = productJson.basic.name;
+        this.price = new Price(productJson.store.price);
+    }
+}
+
+class Price {
+    constructor(priceJson) {
+        this.unit_price = parseFloat(priceJson.unit);
+        this.unitOfMeasure = priceJson.salesUnitOfMeasure;
+        this.salesQuantity = parseFloat(priceJson.salesQuantity);
+        // Unit * sale_quantity = list price
+        this.list_price = priceJson.list;
+        // amount paid per X (Each, oz, fl oz, lb, etc)
+        this.priceUnitOfMeasure = priceJson.priceUnitOfMeasure;
+    }
+}
