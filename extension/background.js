7deb1d0979f2b9f691abf32e01a9ccf1622896f4
Alex Gurganus <gurgalex@iastate.edu>
2020-05-11 17:57:34 -0500

    some progress on using debugger does not work when browsing items on page. Only on refresh. No responseBody

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,16 @@
 "use strict";
+
+window.addEventListener("unload", function() {
+        let currTab = tabs[0];
+        if (currTab) { // Sanity check
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.debugger.detach({tabId:currTab.id});
+                console.log("detached debugger from tab id:", currTab.id);
+            });
+        }
+});
+
+
 // Attach debugger every time a tab is updated.
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status !== "complete") {
@@ -11,6 +23,10 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     chrome.debugger.onEvent.addListener(onEvent);
 });
 
+
+//requestIds
+let pendingRequests = [];
+
 /**
  * Handles any events that are triggered by Chrome debugger
  * @param debuggeeId {chrome.debugger.types.Debuggee} Most concenred with tabId (for content tab)
@@ -18,19 +34,48 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
  * @param params {Object} JSON object with the parameters. Structure of the parameters varies depending on the method name and is defined by the 'parameters' attribute of the event description in the remote debugging protocol.
  */
 function onEvent(debuggeeId, message, params) {
+
+    //console.log("got event", message, params);
     if (message == "Network.responseReceived") {
         if (params.response.url.includes("grocery.walmart.com/v3/api/products")) {
+            //console.log(debuggeeId, message, params);
+            pendingRequests.push(params.requestId);
+            //console.log("saved pending requestID:", params.requestId);
+        }
+    }
+    else if (message == "Network.loadingFinished") {
+        //console.log("loading event:", message, params);
+        let indexOrNot = pendingRequests.indexOf(params.requestId);
+        if (indexOrNot === -1) {
+            return;
+        }
+        // Got a grocery price request
+        let neededRequestId = pendingRequests[indexOrNot];
+
+        // Check if the loadingFinished event is for this request
+        if (neededRequestId !== params.requestId) {
+            console.log(`pendingRequestID ${neededRequestId} !== ${params.requestId} loadFinished requestId`);
+            return;
+        }
 
             // Copied from: https://stackoverflow.com/questions/48785946/how-to-get-response-body-of-all-requests-made-in-a-chrome-extension
+        // encoded data length is -1 or 0
+        console.log("requesting response for requestID:", neededRequestId);
             chrome.debugger.sendCommand({
                 tabId: debuggeeId.tabId
             }, "Network.getResponseBody", {
-                "requestId": params.requestId
+                "requestId": neededRequestId
             }, function (response) {
+
+                if (!response) {
+                    console.log("no response", response);
+                    return;
+                }
+                console.log("captured resp:", response);
                 let response_json = JSON.parse(response.body);
                 parseProductJson(response_json);
-            });
-        }
+            })
+        pendingRequests.splice(indexOrNot, 1);
     }
 }
 
